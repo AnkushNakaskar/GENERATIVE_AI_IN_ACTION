5db7ad624fcc00bc52d8e4127fcab146151973c5
Ankush Nakaskar <ankush.nakaskar@phonepe.com>
2025-06-13 12:21:20 +0530

    Added api key changes and invoke

--- actual_open_ai/actual_open_api_env.py
+++ actual_open_ai/actual_open_api_env.py
@@ -1,19 +1,17 @@
 from openai import OpenAI
+import httpx
+# Get the API from gmail ankush.nakaskar@gmail.com Or from : https://platform.openai.com/settings/organization/api-keys
+api_key=""
+http_client = httpx.Client(verify=False)
+client = OpenAI(base_url="https://api.openai.com/v1", api_key=api_key, http_client=http_client)
 
-client = OpenAI(
-  api_key=""
-)
 
 completion = client.chat.completions.create(
   model="gpt-4o-mini",
   store=True,
   messages=[
-    {"role": "user", "content": "write a haiku about ai"}
+    {"role": "user", "content": "Who is CEO of microsoft ?"}
   ]
 )
-
+print(completion)
 print(completion.choices[0].message);
-
-
-
-
